b034f5f56a8ebd85bac326a62fc48ce1a0b0237a
Balanambi <balanambi.s@gmail.com>
2024-07-18 22:20:31 +0530

    Update CustomAssemblyLoadContext.cs

--- CustomAssemblyLoadContext.cs
+++ CustomAssemblyLoadContext.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
@@ -8,6 +9,7 @@ namespace DllDependencyExtractor
     public class CustomAssemblyLoadContext : AssemblyLoadContext
     {
         private readonly string _folderPath;
+        private readonly HashSet<string> _loadedAssemblies = new HashSet<string>();
 
         public CustomAssemblyLoadContext(string folderPath)
         {
@@ -19,9 +21,44 @@ namespace DllDependencyExtractor
             string assemblyPath = Path.Combine(_folderPath, $"{assemblyName.Name}.dll");
             if (File.Exists(assemblyPath))
             {
-                return LoadFromAssemblyPath(assemblyPath);
+                return LoadAssemblyAndDependencies(assemblyPath);
             }
-           return Default.LoadFromAssemblyName(assemblyName);
+
+            // Try to load the assembly from the default context
+            try
+            {
+                return Default.LoadFromAssemblyName(assemblyName);
+            }
+            catch (FileNotFoundException)
+            {
+                // Log that the assembly was not found
+                Logger.Error($"Assembly '{assemblyName.Name}' could not be found.");
+                return null;
+            }
+        }
+
+        private Assembly LoadAssemblyAndDependencies(string assemblyPath)
+        {
+            string assemblyName = Path.GetFileNameWithoutExtension(assemblyPath);
+            if (_loadedAssemblies.Contains(assemblyName))
+            {
+                return null;
+            }
+
+            Logger.Info($"Loading assembly: {assemblyPath}");
+            var assembly = LoadFromAssemblyPath(assemblyPath);
+            _loadedAssemblies.Add(assemblyName);
+
+            // Load dependencies recursively
+            foreach (var referencedAssembly in assembly.GetReferencedAssemblies())
+            {
+                if (!_loadedAssemblies.Contains(referencedAssembly.Name))
+                {
+                    Load(referencedAssembly);
+                }
+            }
+
+            return assembly;
         }
     }
 }
